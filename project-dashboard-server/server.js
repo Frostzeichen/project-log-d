3fc4678ec0f8c9aa76291f82eb58e7867e99aa60
Frostzeichen <cruztjomar@gmail.com>
2024-04-18 08:47:10 +0800

    add model code to controller

--- project-dashboard-server/server.js
+++ project-dashboard-server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const d = require("./log.d");
 const mysql = require("mysql2");
+
 require("dotenv").config();
 
 const app = express();
@@ -7,26 +9,27 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    multipleStatements: true // Allows multiple statements in one query.
 });
-
 connection.connect();
-
-connection.query('SELECT * FROM inputs', (err, rows, fields) => {
-    if (err) throw err
-    console.log(fields, rows)
-});
-
-connection.end();
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.use(express.json());
-
+ 
 app.get("/", (req, res) => {
    res.render("admin/index.ejs");
 });
 
+app.post("/log.d/receive", (req, res) => {
+    const logd = d.migrateQUery(req.body);
+    const query = mysql.format(logd.query, logd.data);
 
+    connection.query(query, (err) => {
+        if (err) throw err;
+    });
+    res.status(200).send("OK");
+});
 
 app.listen(process.env.SV_PORT);
